a390f809e1f2814ab8c6f7cd1eee55b6f2876369
linth <zhua0404@gmail.com>
2016-05-30 21:18:20 -0700

    add a new function for calcuating the total area.

--- prog12_1.cc
+++ prog12_1.cc
@@ -8,13 +8,20 @@ public:
 	char id;
 	int width;
 	int height;
+	int numberOfWindowns;
 	
 	int area();
+	inline int totalArea();
 };
 
 int CWin::area()
 {
 	return width * height;
+}
+
+inline int CWin::totalArea()
+{
+	return area() * numberOfWindowns;
 }
 
 int main(int argc, char const *argv[])
@@ -25,8 +32,10 @@ int main(int argc, char const *argv[])
 	win1.id = 'A';
 	win1.width = 50; 
 	win1.height = 40;
+	win1.numberOfWindowns = 2;
 
 	cout << "Windows " << win1.id << ", area= " << win1.area() << endl;
+	cout << "Totla Area: " << win1.totalArea() << endl;
 
 	return 0;
 }
