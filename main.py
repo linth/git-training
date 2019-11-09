a55259242b1ecee019a32928d350cf0a03eaaed4
linth <zhua0404@gmail.com>
2019-11-09 16:58:29 +0800

    add a function: mul to calculating.

--- main.py
+++ main.py
@@ -11,8 +11,14 @@ def show_version():
 def pos(a, b):
     return a-b
 
+
+def mul(a, b):
+    return a*b
+
+
 if __name__ == '__main__':
     main()
     show_version()
 
     pos(1, 2)
+    mul(2, 3)
