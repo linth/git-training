4550c75c4a332e9e9b7d21a96312312d65efd9fb
linth <zhua0404@gmail.com>
2019-11-09 16:49:11 +0800

    add a new function for show_a_new_branch.

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ def show_version():
     print('This is a branch with a new function.')
 
 
+def show_a_new_branch():
+    print('This is a f2 branch.')
+
+
 if __name__ == '__main__':
     main()
     show_version()
+    show_a_new_branch()
